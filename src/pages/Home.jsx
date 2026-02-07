f8f2b37630c77ac600d1b8e084a5dc8427a81cd3
ZFO <harsh@zfo.co.in>
2026-02-08 00:14:31 +0530

    fix: restore missing component imports in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,11 @@
 import SEO from "../components/SEO";
+import Hero from "../components/Hero";
+import PromoSection from "../components/PromoSection";
+import LaunchTeaser from "../components/LaunchTeaser";
+import HeroScene from "../components/HeroScene";
+import ProductDetails from "../components/ProductDetails";
+import FizzroomTeaser from "../components/FizzroomTeaser";
+import SEOFeatures from "../components/SEOFeatures";
 
 const Home = () => {
     return (
