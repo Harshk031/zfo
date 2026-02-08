f688379e830d176d4b906f402c2eed2c39e83f55
ZFO <harsh@zfo.co.in>
2026-02-08 16:05:09 +0530

    Apply mix-blend-mode darken to remove white backgrounds

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -52,6 +52,10 @@ const ProductDetails = () => {
                     src={prod.image}
                     alt="ZfO Premium Craft Soda in Glass Bottle - Authentic Masala Soda with Natural Ingredients"
                     className="relative object-contain h-[120%] -rotate-12 hover:rotate-0 transition-transform duration-700 ease-[cubic-bezier(0.22,1,0.36,1)]"
+                    style={{
+                      mixBlendMode: 'darken',
+                      filter: 'contrast(1.1) brightness(1.05)'
+                    }}
                   />
                 </div>
               </div>
