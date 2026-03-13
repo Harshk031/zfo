b4181db283a4820cb99c3f30952b8aa4592b926f
Harsh Katiyar <beverages@zfo.co.in>
2026-03-13 19:14:20 +0530

    seo: added /order to sitemap, removed old static sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -18,6 +18,12 @@ export default async function sitemap() {
             priority: 0.9,
         },
         {
+            url: `${baseUrl}/order`,
+            lastModified: new Date(),
+            changeFrequency: 'always',
+            priority: 1.0,
+        },
+        {
             url: `${baseUrl}/about`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
